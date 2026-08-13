f5d277d2d4ba8dc1a14de6aab809915e66cd190b
SrJorgeG <sr.g.jorge26@gmail.com>
2026-08-13 12:20:01 +0200

    6

--- exam_rank_5/level_2/game_of_life/lifetry.c
+++ exam_rank_5/level_2/game_of_life/lifetry.c
@@ -2,72 +2,84 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct s_info
+typedef struct s_life
 {
-	int		width;
-	int		height;
-	int		iterations;
-	char	*board;
-} t_info;
+	int height;
+	int width;
+	int iterations;
+	char *board;
+} t_life;
 
-int	init_board(t_info *t, char **av)
+void	print_board(t_life t)
 {
-	t->width = atoi(av[0]);
-	t->height = atoi(av[1]);
+	for (int x = 0; x < t.height; x++)
+	{
+		for (int y = 0; y < t.width; y++)
+			putchar((t.board[x * t.width + y] ? '0' : ' '));
+		putchar('\n');
+	}
+}
+
+int init_board(t_life *t, char **av)
+{
+	t->height = atoi(av[0]);
+	t->width = atoi(av[1]);
 	t->iterations = atoi(av[2]);
-	if (t->width <= 0 || t->height <= 0 || t->iterations < 0)
-		return (1);	
+	if (t->height <= 0 || t->width <= 0 || t->iterations < 0)
+		return 1;
 	t->board = calloc(t->width * t->height, 1);
 	return (t->board == NULL);
 }
 
-int set_board(t_info *t)
+int set(t_life *t)
 {
 	char buf;
 	int flag = 0, x = 0, y = 0;
-	while (read(STDIN_FILENO, &buf, 1))
+	while(read(STDIN_FILENO, &buf, 1) == 1)
 	{
 		if (buf == 'w' && x > 0)
 			x--;
-		else if (buf == 's' && x < t->height)
+		else if (buf == 's' && x < t->height - 1)
 			x++;
 		else if (buf == 'a' && y > 0)
 			y--;
-		else if (buf == 'd' && y < t->height)
+		else if (buf == 'd' && y < t->width - 1)
 			y++;
 		else if (buf == 'x')
 			flag = !flag;
 		else if (buf != '\n')
 			return (1);
 		if (flag)
-			t->board[x * t->width + y];
+			t->board[x * t->width + y] = 1;
 	}
 	return (0);
 }
 
-int count_neighbours(t_info t, int x, int y)
+int count_neighbours(t_life t, int x, int y)
 {
-	int n = 0, dx, dy, nx, ny;
+	int nx, ny, dx, dy, n = 0;
 	for (dx = -1; dx <= 1; dx++)
 		for (dy = -1; dy <= 1; dy++)
 		{
+			if (dx == 0 && dy == 0)
+				continue;
 			nx = x + dx;
 			ny = y + dy;
-			if (nx > 0 && nx < t.width && ny > 0 && ny < t.height && t.board[nx * t.width + ny])
+			if (nx >= 0 && nx < t.height && ny >= 0 && ny < t.width && t.board[nx * t.width + ny])
 				n++;
 		}
-	return (n);	
-}
+	return n;
+}	
 
-void iterate_game(t_info *t)
+void iterate_game(t_life *t)
 {
-	int neigboard, x, y;
+	int x, y, nigboard;
 	char *next = calloc(t->width * t->height, 1);
 	for (x = 0; x < t->height; x++)
-		for (y = 0; y < t->width; y++)
+		for (int y = 0; y < t->width; y++)
 		{
-			neigboard = count_neighbours(*t, x, y);
-			next[x * t->width + y] = (neigboard == 3 || (t->board[x * t->width + y] && neigboard == 2));
+			nigboard = count_neighbours(*t, x, y);
+			next[x * t->width + y] = (nigboard == 3 || (t->board[x * t->width + y] && nigboard == 2));
 		}
 	free(t->board);
 	t->board = next;
@@ -75,16 +87,16 @@ void iterate_game(t_info *t)
 
 int main(int ac, char **av)
 {
-	t_info t;
+	t_life t;
 	if (ac != 4)
-		return (1);
-	if (init_board(&t, &av[1]))
-		return (2);
-	if (set_board(&t))
+		return 1;
+	if (init_board(&t, av + 1))
+		return 2;
+	if (set(&t))
 		return (free(t.board), 3);
 	for (int i = 0; i < t.iterations; i++)
 		iterate_game(&t);
-	
-	return (0);
-	
+	print_board(t);
+	free(t.board);
+	return (0);		
 }
--- exam_rank_6/mini_serv2.c
+++ exam_rank_6/mini_serv2.c
@@ -16,7 +16,7 @@ int extract_message(char **buf, char **msg)
 	if (*buf == 0)
 		return (0);
 	i = 0;
-	while ((*buf)[i])
+	while ((*buf)[++i])
 	{
 		if ((*buf)[i] == '\n')
 		{
@@ -29,7 +29,6 @@ int extract_message(char **buf, char **msg)
 			*buf = newbuf;
 			return (1);
 		}
-		i++;
 	}
 	return (0);
 }
@@ -54,14 +53,9 @@ char *str_join(char *buf, char *add)
 	return (newbuf);
 }
 
-int count = 0;
-int max_fd = 0;
-int ids[65536];
-char *msgs[65536];
-
+int count = 0, max_fd = 0, ids[65536];
+char *msgs[65536], buf_read[1001], buf_write[42];
 fd_set read_fds, write_fds, all_fds;
-
-char buf_read[1001], buf_write[42];
 
 void fatal_error()
 {
@@ -80,134 +74,54 @@ int create_socket()
 
 void notify_other(int author, char *str)
 {
-    for (int fd = 0; fd <= max_fd; fd++)
-    {
-        if (FD_ISSET(fd, &write_fds) && fd != author)
-        {
-            send(fd, str, strlen(str), 0);
-        }
-    }
-}
-
-void register_client(int fd)
-{
-    if(fd > max_fd)
-        max_fd = fd;
-    
-    ids[fd] = count++;
-    msgs[fd] = NULL;
-    FD_SET(fd, &all_fds);
-
-    sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
-    notify_other(fd, buf_write);
-}
-
-void remove_client(int fd)
-{
-    sprintf(buf_write, "server: client %d just left\n", ids[fd]);
-    notify_other(fd, buf_write);
-
-    free(msgs[fd]);
-    FD_CLR(fd, &all_fds);
-    close(fd);
+	for (int fd = 0; fd <= max_fd; fd++)
+		if (FD_ISSET(fd, &write_fds) && fd != author)
+			send(fd, str, strlen(str), 0);
 }
 
 void send_msg(int fd)
 {
-    char *msg;
+	char *msg;
     while(extract_message(&(msgs[fd]), &msg))
     {
-        sprintf(buf_write, "client %d: ", ids[fd]);
+		sprintf(buf_write, "client %d: ", ids[fd]);
         notify_other(fd, buf_write);
-
         notify_other(fd, msg);
         free(msg);
     }
 }
 
-// int main(int argc, char **argv) {
+void register_client(int fd)
+{
+	if(fd > max_fd)
+		max_fd = fd;
+	ids[fd] = count++;
+	msgs[fd] = NULL;
+	FD_SET(fd, &all_fds);
+	sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
+	notify_other(fd, buf_write);
+}
 
-// 	if (argc != 2)
-//     {
-// 		write(2, "Wrong number of arguments\n", 26);
-// 		exit (1);
-// 	}
-
-// 	FD_ZERO(&all_fds);
-// 	int sockfd = create_socket();
-
-// 	struct sockaddr_in servaddr;
-// 	bzero(&servaddr, sizeof(servaddr));
-	
-// 	servaddr.sin_family = AF_INET;
-// 	servaddr.sin_addr.s_addr = htonl(2130706433);
-// 	servaddr.sin_port = htons(atoi(argv[1]));
-
-// 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-//     {
-// 		fatal_error();
-// 	}
-// 	if (listen(sockfd, 128) != 0)
-//     {
-// 		fatal_error();
-// 	}
-
-// 	while (1)
-//     {
-// 		read_fds = write_fds = all_fds;
-// 		if (select(max_fd + 1, &read_fds, &write_fds, NULL, NULL) < 0)
-//         {
-// 			fatal_error();
-// 		}
-
-// 		for (int fd = 0; fd <= max_fd; fd++)
-//         {
-// 			if (!FD_ISSET(fd, &read_fds))
-// 				continue;
-
-// 			if (fd == sockfd){
-// 				socklen_t addr_len = sizeof(servaddr);
-// 				int client_fd = accept(sockfd, (struct sockaddr *)&servaddr, &addr_len);
-// 				if (client_fd >= 0 )
-//                 {
-// 					register_client(client_fd);
-// 					break;
-// 				}
-// 			}
-// 			else
-//             {
-// 				int bytes_read = recv(fd, buf_read, 1000, 0);
-// 				if (bytes_read <= 0)
-//                 {
-// 					remove_client(fd);
-// 					break;
-// 				}
-// 				buf_read[bytes_read] = '\0';
-// 				msgs[fd] = str_join(msgs[fd], buf_read);
-// 				send_msg(fd);
-// 			}
-// 		}
-// 	}
-// 	return 0;
-// }
+void remove_client(int fd)
+{
+	sprintf(buf_write, "server: client %d just left\n", ids[fd]);
+	notify_other(fd, buf_write);
+	free(msgs[fd]);
+	FD_CLR(fd, &all_fds);
+	close(fd);
+}
 
 int main(int ac, char *av[])
 {
 	if (ac != 2)
-	{
 		write(2, "Wrong number of arguments\n", 26);
-	}
-
 	FD_ZERO(&all_fds);
 	int sockfd = create_socket();
-
 	struct sockaddr_in servaddr;
 	bzero(&servaddr, sizeof(servaddr));
-
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
 	servaddr.sin_port = htons(atoi(av[1]));
-
 	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
 		fatal_error();
 	if (listen(sockfd, 128) != 0)
